a247e1f4e48b0fa9146d9ad4712934d5054e1dd2
Stefania Giannopoulou <stefania.gian91@gmail.com>
2019-10-17 15:52:55 +0300

    Add files via upload

---
+++ Todo List jQuery/script.js
@@ -0,0 +1,14 @@
+//check off todos by clicking
+$("li").click(function(){
+  $(this).toggleClass("completed");
+});
+
+
+//click on X to delete todo
+$("span").click(function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
